9bcd99787b7f8e45ddef550b4cb1b5aadae69432
Liz Towoju <liztowoju@gmail.com>
2022-08-19 21:47:07 +0100

    added error message with inner html and foreach loop

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let formElement = document.getElementById("login-form")
+let errors = [];
 
 function validatePhoneOrEmail(inputEl) {
     let phoneFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
@@ -7,7 +8,7 @@ function validatePhoneOrEmail(inputEl) {
     if (inputEl.value.match(phoneFormat) || inputEl.value.match(emailFormat)) {
         return true;
     } else {
-        alert("Please enter a valid email or phone number. Phone number shouldn't be more than 10 digits");
+        errors.push("Please enter a valid email or phone number. Phone number shouldn't be more than 10 digits");
         return false;
     }
 }
@@ -16,32 +17,50 @@ function validatePassword(pw) {
 
     //check empty password field  
     if (pw == "") {
-        alert( "**Fill the password please!");
+        errors.push( "**Fill the password please!");
         return false;
     }
 
     //minimum password length validation  
     if (pw.length < 8) {
-        alert( "**Password length must be atleast 8 characters" );
+        errors.push( "**Password length must be atleast 8 characters" );
         return false;
     }
 
     //maximum length of password validation  
     if (pw.length > 15) {
-        alert( "**Password length must not exceed 15 characters" );
+        errors.push( "**Password length must not exceed 15 characters" );
         return false;
     } else {
-        alert("Password is correct");
+        return true;
     }
 }
 
 formElement.addEventListener('submit', function (event) {
     event.preventDefault();
 
+    errors = [];
+
     let emailOrPhone = document.getElementById("email_or_phone");
     validatePhoneOrEmail(emailOrPhone);
+
     let password = document.getElementById("password").value;
     validatePassword(password);
 
+    if (errors.length > 0){
+        // alert( errors[0] );
+        // console.log(document.querySelector('form-errors'));
+        // document.querySelector('#form-errors').innerHTML = errors;
 
+        let output = "<ol>";
+        errors.forEach(function(item){
+            output += '<li>' + item + '</li>';
+        });
+        // output += '<li>' + errors[0] + '</li>';
+        output += "</ol>";
+
+        document.querySelector('#form-errors').innerHTML = output;
+        // console.log(output);
+    }
+    // console.log(errors)
 })
